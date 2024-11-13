76c3a46f9d793e170ae5c2f4fcc894e35dff621b
YuanYuanOwO <81153017+YuanYuanOwO@users.noreply.github.com>
2024-11-13 23:56:58 +0800

    Update SubCommand_Limit.java

--- addon/limited/src/main/java/com/ghostchu/quickshop/addon/limited/command/SubCommand_Limit.java
+++ addon/limited/src/main/java/com/ghostchu/quickshop/addon/limited/command/SubCommand_Limit.java
@@ -32,11 +32,11 @@ public class SubCommand_Limit implements CommandHandler<Player> {
       quickshop.text().of(sender, "command.wrong-args").send();
       return;
     }
-    if (shop.getOwner().getUniqueId() != sender.getUniqueId()) {
+    final Shop shop = getLookingShop(sender);
+        if (shop.getOwner().getUniqueId() != sender.getUniqueId()) {
             quickshop.text().of(sender, "not-managed-shop").send();
             return;
     }
-    final Shop shop = getLookingShop(sender);
     if(shop == null) {
       quickshop.text().of(sender, "not-looking-at-shop").send();
       return;
